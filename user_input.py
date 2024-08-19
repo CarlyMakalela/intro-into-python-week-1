bb30062fecf472478833f3fba6c085ed95d70810
Carly Makalela <tshiamomakalela@gmail.com>
2024-08-19 18:33:00 +0200

    assignment done

---
+++ user_input.py
@@ -0,0 +1,9 @@
+print("Welcome! Let's create a personalized message for you.")
+
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+print(f"Hello {name}, you are only {age} years old and living the dream in {location}.")
+
+
